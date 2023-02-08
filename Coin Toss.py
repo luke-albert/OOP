79674de53493ab84b6cff5b74f70b54b50940a72
Luke <luke_albert1@baylor.edu>
2023-02-08 14:13:43 -0600

    class changes

--- Coin Toss.py
+++ Coin Toss.py
@@ -1,27 +1,27 @@
+# i am inporting the name of the file and not the class(the name of the class is coin)
 import CoinClass as c
 
 
 # The main function.
 def main():
-       # Create an object from the Coin class.
-       my_coin = c.Coin()   # this creates an instance called 'my_coin' of the class 'Coin()'
+    # Create an object from the Coin class.
+    my_coin = c.Coin()   # this creates an instance called 'my_coin' of the class 'Coin()'
 
-       # Display the side of the coin that is facing up.
-       print('This side is up:', my_coin.get_sideup())    # notice you do not have to supply the argument/parameter
+    # Display the side of the coin that is facing up.
+    # notice you do not have to supply the argument/parameter; make sure you use the variable and not c.Coin()
+    print('This side is up:', my_coin.get_sideup())
 
-       # Toss the coin.
-       print('I am going to toss the coin ten times:')
-       for count in range(10):
-           my_coin.toss()
-           
-           # Display the side of the coin that is facing up.
-           print('This side is up:', my_coin.get_sideup())
+    # Toss the coin.
+    print('I am going to toss the coin ten times:')
+    for count in range(10):
+        my_coin.toss()
 
-           
+        # Display the side of the coin that is facing up.
+        print('This side is up:', my_coin.get_sideup())
 
-
-       
 
 # Call the main function.
-
 main()
+
+
+# this is the file that we will actually run
